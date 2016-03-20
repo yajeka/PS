15cb4272bdb2316d3d5423341bac250ae8c7a676
akoyro <pervoliner@gmail.com>
2016-03-20 22:33:39 +1100

    signUp implemented

--- PS/BackendManager.swift
+++ PS/BackendManager.swift
@@ -18,31 +18,40 @@ public class BackendManager {
             block!(success: nil, error: result as? String)
         } else if  let input = result as? NSDictionary {
             let query = PFQuery(className: className);
-            do {
-                let objectId = input["objectId"] as! String
-                
-                let task = query.getObjectInBackgroundWithId(objectId)
-                
-                task.waitUntilFinished()
-                
-                let object:PFObject = task.result as! PFObject
-                
-                block!(success: object, error: nil)
-            } catch {
-                block!(success: nil, error: "error")
-            }
+            let objectId = input["objectId"] as! String
+            
+            let task = query.getObjectInBackgroundWithId(objectId)
+            
+            task.waitUntilFinished()
+            
+            let object:PFObject = task.result as! PFObject
+            
+            block!(success: object, error: nil)
         }
     }
     
     public class func signUp(uid: String, email: String, password: String,  block: BackedBlock?) {
+        findByEmailAndPassword(email, password: password, block: { (success: PFObject?, error: String?) -> Void in
+            if ((error) != nil) {
+                let account = PFObject(className: "Account")
+                account["uids"] = [uid]
+                account["email"] = email
+                account["password"] = password
+                let task = account.saveInBackground()
+                task.waitUntilFinished()
+                block!(success: account, error: nil)
+            } else {
+                block!(success: nil, error: "account_exists")
+            }
+        })
     }
     
     public class func findByEmailAndPassword(email: String, password: String,  block: BackedBlock?) {
         PFCloud.callFunctionInBackground("findByEmailAndPassword", withParameters: ["email": email, "password": password], block: {(result: AnyObject? , error: NSError?) -> Void in
-                handleResult(result,error: error, block: block, className: "Account")
-            });
+            handleResult(result,error: error, block: block, className: "Account")
+        });
     }
-
+    
     
     public class func findByUid(uid: String, block: BackedBlock?) {
         PFCloud.callFunctionInBackground("findByUid", withParameters: ["uid": uid], block: {(result: AnyObject? , error: NSError?) -> Void in
@@ -53,8 +62,8 @@ public class BackendManager {
     public class func findByEmailAndPassword(email: String, password: String) -> BFTask {
         return PFCloud.callFunctionInBackground("findByEmailAndPassword", withParameters: ["email": email, "password": password]);
     }
-
-
+    
+    
     private class func findByUid(uid: String) -> BFTask {
         return PFCloud.callFunctionInBackground("findByUid", withParameters: ["uid": uid]);
     }
