0741da33e2f6acfd0fcd1bc1713dcca0cf9f4eb4
Aliaksandr Kantsevoi <powerscin@gmail.com>
2016-03-20 12:22:57 +0300

    Add mainViewModel

---
+++ PS/MainViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  MainViewModel.swift
+//  PS
+//
+//  Created by Alexandr on 20.03.16.
+//  Copyright © 2016 hackathon. All rights reserved.
+//
+
+import UIKit
+
+class MainViewModel: NSObject {
+
+    internal func runMainModel() {
+        self.checkAccount()
+    }
+    
+    private func checkAccount () {
+    
+    }
+    
+}
