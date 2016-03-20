90666ef669c3abd62e18ec62c63a29dcbe205774
Andrew Rudsky <andy.rudsky@gmail.com>
2016-03-20 13:40:00 +0300

    View controller backbone for Master storyboard

---
+++ PS/Master/CreatePostViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CreatePostViewController.swift
+//  PS
+//
+//  Created by Andrew Rudsky on 3/20/16.
+//  Copyright © 2016 hackathon. All rights reserved.
+//
+
+import UIKit
+
+class CreatePostViewController : UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad();
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning();
+    }
+}
